8297f0c754db232a3fd218cb94d34029c4f188fd
bmerckling <brandon.merckling@gmail.com>
2022-04-23 19:07:32 -0400

    updating images

--- js/main.js
+++ js/main.js
@@ -138,48 +138,48 @@
               $folioItems = $('.folio-item');
 
         // get items
-        $folioItems.each( function(i) {
+        // $folioItems.each( function(i) {
 
-            let $folio = $(this),
-                $thumbLink =  $folio.find('.folio-item__thumb-link'),
-                $title = $folio.find('.folio-item__title'),
-                $caption = $folio.find('.folio-item__caption'),
-                $titleText = '<h4>' + $.trim($title.html()) + '</h4>',
-                $captionText = $.trim($caption.html()),
-                $href = $thumbLink.attr('href'),
-                $size = $thumbLink.data('size').split('x'),
-                $width  = $size[0],
-                $height = $size[1];
+        //     let $folio = $(this),
+        //         $thumbLink =  $folio.find('.folio-item__thumb-link'),
+        //         $title = $folio.find('.folio-item__title'),
+        //         $caption = $folio.find('.folio-item__caption'),
+        //         $titleText = '<h4>' + $.trim($title.html()) + '</h4>',
+        //         $captionText = $.trim($caption.html()),
+        //         $href = $thumbLink.attr('href'),
+        //         $size = $thumbLink.data('size').split('x'),
+        //         $width  = $size[0],
+        //         $height = $size[1];
         
-            let item = {
-                src  : $href,
-                w    : $width,
-                h    : $height
-            }
+        //     let item = {
+        //         src  : $href,
+        //         w    : $width,
+        //         h    : $height
+        //     }
 
-            if ($caption.length > 0) {
-                item.title = $.trim($titleText + $captionText);
-            }
+        //     if ($caption.length > 0) {
+        //         item.title = $.trim($titleText + $captionText);
+        //     }
 
-            items.push(item);
-        });
+        //     items.push(item);
+        // });
 
-        // bind click event
-        $folioItems.each(function(i) {
+        // // bind click event
+        // $folioItems.each(function(i) {
 
-            $(this).find('.folio-item__thumb-link').on('click', function(e) {
-                e.preventDefault();
-                let options = {
-                    index: i,
-                    showHideOpacity: true
-                }
+        //     $(this).find('.folio-item__thumb-link').on('click', function(e) {
+        //         e.preventDefault();
+        //         let options = {
+        //             index: i,
+        //             showHideOpacity: true
+        //         }
 
-                // initialize PhotoSwipe
-                let lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
-                lightBox.init();
-            });
+        //         // initialize PhotoSwipe
+        //         let lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+        //         lightBox.init();
+        //     });
 
-        });
+        // });
     };
 
 
